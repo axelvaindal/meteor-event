43eeadaf6815a06b25a7773655c21cefd8cfb6f3
Axel Vaindal <pro.axelvaindal@gmail.com>
2019-02-03 16:21:14 +0100

    fix error

--- package.js
+++ package.js
@@ -1,24 +1,21 @@
-Package.describe(
-{
-    name: "m4dnation:meteor-event",
+Package.describe({
+  name: "m4dnation:meteor-event",
 
-    version: "3.0.0",
+  version: "3.0.0-alpha.1",
 
-    summary: "Event package for MeteorJS framework.",
+  summary: "Event package for MeteorJS framework.",
 
-    git: "https://github.com/M4dNation/meteor-event",
+  git: "https://github.com/M4dNation/meteor-event",
 });
 
-Npm.depends(
-{
-    "jstoolbox": "1.0.2"
+Npm.depends({
+  jstoolbox: "1.0.2",
 });
 
-Package.onUse(function(api) 
-{
-	api.versionsFrom("1.8.0.2");
+Package.onUse(function(api) {
+  api.versionsFrom("1.8.0.2");
 
-    api.use(["ecmascript"]);
+  api.use(["ecmascript"]);
 
-    api.mainModule("src/meteor-event.js", "server");
+  api.mainModule("src/meteor-event.js", "server");
 });
